d3890af983763115135b9f2f08aa1370e7861526
stefanolehnen <stefanolhn@gmail.com>
2022-08-23 13:03:42 -0300

    ALteração feita na parte de login, informando retornos de sucesso e falha

--- app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,16 +1,20 @@
 package com.example.tasks.service.repository
 
 import android.app.Person
+import android.content.Context
+import com.example.tasks.R
+import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.model.PersonModel
 import com.example.tasks.service.model.PriorityModel
 import com.example.tasks.service.repository.remote.PersonService
 import com.example.tasks.service.repository.remote.RetrofitClient
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PersonRepository {
+class PersonRepository(val context: Context) {
 
     val remote = RetrofitClient.getService(PersonService::class.java)
 
@@ -20,17 +24,22 @@ class PersonRepository {
         call.enqueue(object: Callback<PersonModel>{
             override fun onResponse(call: Call<PersonModel>, response: Response<PersonModel>) {
                 val s = ""
-                if(response.code() == 200){
+                if(response.code() == TaskConstants.HTTP.SUCCESS){
                     response.body()?.let{ listener.onSuccess(it)}
                 }else{
-                    listener.onFailure(response.errorBody()!!.toString())
+                    listener.onFailure(failResponse(response.errorBody()!!.string()))
                 }
             }
 
             override fun onFailure(call: Call<PersonModel>, t: Throwable) {
                 val s = ""
-                listener.onFailure("Erro Inesperado")
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
             }
         })
+    }
+
+    private fun failResponse(str: String): String{
+        return Gson().fromJson(str,String::class.java)
+
     }
 }
--- app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,6 +2,8 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.model.PersonModel
 import com.example.tasks.service.repository.PersonRepository
@@ -9,19 +11,23 @@ import com.example.tasks.service.repository.PersonRepository
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
 
-    private val personRepository = PersonRepository()
-    /**
-     * Faz login usando API
-     */
+    private val personRepository = PersonRepository(application.applicationContext)
+
+    private val _login = MutableLiveData<PersonModel>()
+    val login: LiveData<PersonModel> = _login
+
+    private val _failure = MutableLiveData<String>()
+    val failure: LiveData<String> = _failure
+
     fun doLogin(email: String, password: String) {
 
         personRepository.login(email,password, object : APIListener<PersonModel> {
             override fun onSuccess(result: PersonModel) {
-
+                _login.value = result
             }
 
             override fun onFailure(message: String) {
-
+                _failure.value = message
             }
         })
 
